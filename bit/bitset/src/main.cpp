5e2fcc2efdedcd40579efe660c6bd7106570ccfa
podumai <kirillsm05@gmail.com>
2024-11-03 23:15:00 +0700

    Improved bitset

---
+++ bit/bitset/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "bitset.hpp"
+
+npl::i32 main()
+{
+  npl::bit::bitset<16> test (0xff);
+  
+  for (auto iterator : test)
+    std::cout << iterator;
+  std::cout << '\n';
+  
+  return 0;
+}
