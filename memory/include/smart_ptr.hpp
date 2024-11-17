402146475e48690a2b2b2eeeb665f2bff9ebbc18
podumai <kirillsm05@gmail.com>
2024-11-18 01:48:05 +0700

    New smart pointer class with class strategies (In progress...)

---
+++ memory/include/smart_ptr.hpp
@@ -0,0 +1,192 @@
+#ifndef __NOP_LIB_MEMORY_SMART_PTR__
+#define __NOP_LIB_MEMORY_SMART_PTR__ 1
+
+#include <ostream>
+#include <type_traits>
+#include "types.hpp"
+
+namespace nop
+{
+
+  struct allow_conversion
+  {
+    enum
+    {
+      VOID_POINTER = true,
+      BOOL = true
+    };
+  };
+
+  struct disallow_conversion
+  {
+    enum
+    {
+      VOID_POINTER = false,
+      BOOL = false
+    };
+  };
+
+  template <typename T>
+  struct single_storage 
+  {
+    static T* Create() noexcept
+    {
+      std::cout << "StoragePolicySingle is running...\n";
+      return nullptr;
+    }
+
+    static void Delete(T* pointer) noexcept
+    {
+      std::cout << "StoragePolicySingle is out of date\n";
+      if (pointer != nullptr)
+        delete pointer;
+    }
+
+    enum { OPERATOR_SUBSCRIPT = false };
+  };
+
+  template<typename T>
+  struct multiple_storage
+  {
+    static T* Create(nop::size_t size = 0)
+    {
+      std::cout << "StoragePolicyMultiple is running...\n";
+
+      if (size == 0)
+        return nullptr;
+      else
+        return new T[size];
+    }
+
+    static void Delete(T* pointer) noexcept
+    {
+      std::cout << "StoragePolicyMultiple is out of date...\n";
+      if (pointer != nullptr)
+        delete[] pointer;
+    }
+
+    enum { OPERATOR_SUBSCRIPT = true };
+  };
+
+  template<
+           typename T,
+// TODO: Implement new class-strategies -> Ownership
+// {template<typename> class Ownership = unique_ownership}
+           class Conversion = allow_conversion,
+// TODO: Implement new class-strategies -> Safety
+// {template<typename> class Safety = no_check}
+           template <typename> class Storage = single_storage
+          >
+  class smart_ptr
+  {
+  public:
+    using size_type = nop::size_t;
+    using pointer = T*;
+    using const_pointer = const T*;
+    using reference = T&;
+    using const_reference = const T&;
+
+  private:
+    pointer m_pointer;
+
+  public:
+    smart_ptr()
+      : m_pointer(nullptr)
+    {}
+
+    explicit smart_ptr(pointer ptr)
+      : m_pointer(ptr)
+    {}
+
+    smart_ptr(const smart_ptr&) = delete;
+    smart_ptr(smart_ptr&&) = delete;
+
+    ~smart_ptr()
+    {
+      Storage<T>::Delete(m_pointer);
+    }
+
+    [[nodiscard]] pointer get() noexcept
+    {
+      return m_pointer;
+    }
+
+    [[nodiscard]] const_pointer get() const noexcept
+    {
+      return m_pointer;
+    }
+
+    void reset(pointer ptr = nullptr) noexcept
+    {
+      if (m_pointer != nullptr)
+        Storage<T>::Delete(m_pointer);
+      m_pointer = ptr;
+    }
+
+    pointer release() noexcept
+    {
+      pointer tmp_ptr{m_pointer};
+      m_pointer = nullptr;
+      return tmp_ptr;
+    }
+
+    [[nodiscard]] reference operator*() noexcept
+    {
+      return *m_pointer;
+    }
+
+    [[nodiscard]] const_reference operator*() const noexcept
+    {
+      return *m_pointer;
+    }
+
+    template<typename U = T>
+    [[nodiscard]] std::enable_if_t<std::is_class_v<U>, T>* operator->() noexcept
+    {
+      return m_pointer;
+    }
+
+    template<typename U = T>
+    [[nodiscard]] const std::enable_if_t<std::is_class_v<U>, T>* operator->() const noexcept
+    {
+      return m_pointer;
+    }
+
+    template<typename U = T>
+    [[nodiscard]] std::enable_if_t<Storage<U>::OPERATOR_SUBSCRIPT == true, T>& operator[](size_type index) noexcept
+    {
+      return m_pointer[index];
+    }
+
+    template<typename U = T>
+    [[nodiscard]] const std::enable_if_t<Storage<U>::OPERATOR_SUBSCRIPT == true, T>& operator[](size_type index) const noexcept
+    {
+      return m_pointer[index];
+    }
+
+    smart_ptr& operator=(const smart_ptr&) = delete;
+    smart_ptr& operator=(smart_ptr&&) = delete;
+
+    template<typename U = void*>
+    [[nodiscard]] operator std::enable_if_t<Conversion::VOID_POINTER == true, U>() noexcept
+    {
+      return m_pointer;
+    }
+
+    template<typename U = bool>
+    [[nodiscard]] operator std::enable_if_t<Conversion::BOOL == true, U>() const noexcept
+    {
+      return m_pointer != nullptr;
+    }
+
+    template<typename U = bool>
+    [[nodiscard]] std::enable_if_t<Conversion::BOOL == true, U> operator!() const noexcept
+    {
+      return !m_pointer;
+    }
+    
+  };
+
+}
+
+#endif
