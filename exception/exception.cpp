8b045112370fab93f0a18b3ecc97d5a807abc680
podumai <kirillsm05@gmail.com>
2025-01-09 09:27:47 +0700

    A lot of work.TODO: Set a std::exception as a base

--- exception/exception.cpp
+++ exception/exception.cpp
@@ -3,208 +3,329 @@
 namespace nop /* Begin namespace nop */
 {
 
-  namespace err /* Begin namespace err */
+namespace err /* Begin namespace err */
+{
+
+  /*
+   * Logic error implementation
+   */
+  LogicError::LogicError()
+#if __cplusplus >= 201103L
+    : m_error{"LogicError()"}
+#else
+    : m_error("LogicError()")
+#endif
+  {}
+
+  LogicError::LogicError(const char* errorMessage)
+#if __cplusplus >= 201103L
+    : m_error{errorMessage}
+#else
+    : m_error(errorMessage)
+#endif
+  {}
+
+  LogicError::LogicError(const std::string& errorMessage)
+#if __cplusplus >= 201103L
+    : m_error{errorMessage}
+  {}
+#else
+  LogicError::LogicError(const std::string& errorMessage)
+    : m_error(errorMessage)
+  {}
+
+  LogicError::LogicError(const LogicError& other)
+    : m_error(other.m_error)
+  {}
+#endif
+
+#if __cplusplus >= 201103L
+  LogicError::LogicError(std::string&& errorMessage) __NOP_ATTRIBUTE_NOEXCEPT__
+    : m_error{std::move(errorMessage)}
+  {}
+#endif
+
+  const char* LogicError::what() const __NOP_ATTRIBUTE_NOEXCEPT__
   {
+    return m_error.c_str();
+  }
 
-    /*
-     * Logic error implementation
-     */
-    LogicError::LogicError()
-      : m_error{"LogicError()"}
-    {}
+  i32 LogicError::errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__
+  {
+    return 1;
+  }
 
-    LogicError::LogicError(const char* errorMessage)
-      : m_error{errorMessage}
-    {}
+#if __cplusplus < 201103L
+  LogicError& LogicError::operator=(const LogicError& other)
+  {
+    m_error = other.m_error;
+    return *this;
+  }
+#endif
 
-    LogicError::LogicError(const std::string& errorMessage)
-      : m_error{errorMessage}
-    {}
+  /*
+   * Invalid argument implementation
+   */
+  InvalidArgument::InvalidArgument()
+#if __cplusplus >= 201103L
+    : LogicError{"InvalidArgument()"}
+#else
+    : LogicError("InvalidArgument()")
+#endif
+  {}
 
-    LogicError::LogicError(std::string&& errorMessage) noexcept
-      : m_error{std::move(errorMessage)}
-    {}
+  InvalidArgument::InvalidArgument(const char* errorMessage)
+#if __cplusplus >= 201103L
+    : LogicError{errorMessage}
+#else
+    : LogicError(errorMessage)
+#endif
+  {}
 
-    const char* LogicError::what() const noexcept
-    {
-      return m_error.c_str();
-    }
+  InvalidArgument::InvalidArgument(const std::string& errorMessage)
+#if __cplusplus >= 201103L
+    : LogicError{errorMessage}
+  {}
+#else
+    : LogicError(errorMessage)
+  {}
 
-    i32 LogicError::errorCode() const noexcept
-    {
-      return 1;
-    }
+  InvalidArgument::InvalidArgument(const InvalidArgument& other)
+    : LogicError(other.what())
+  {}
+#endif
 
-    /*
-     * Invalid argument implementation
-     */
-    InvalidArgument::InvalidArgument()
-      : LogicError{"InvalidArgument()"}
-    {}
+#if __cplusplus >= 201103L
+  InvalidArgument::InvalidArgument(std::string&& errorMessage) __NOP_ATTRIBUTE_NOEXCEPT__
+    : LogicError{std::move(errorMessage)}
+  {}
+#endif
 
-    InvalidArgument::InvalidArgument(const char* errorMessage)
-      : LogicError{errorMessage}
-    {}
+  i32 InvalidArgument::errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__
+  {
+    return 2;
+  }
 
-    InvalidArgument::InvalidArgument(const std::string& errorMessage)
-      : LogicError{errorMessage}
-    {}
+#if __cplusplus < 201103L
+  InvalidArgument& InvalidArgument::operator=(const InvalidArgument& other)
+  {
+    return *this;
+  }
+#endif
 
-    InvalidArgument::InvalidArgument(std::string&& errorMessage) noexcept
-      : LogicError{std::move(errorMessage)}
-    {}
+  /*
+   * Runtime error implementation
+   */
+  RuntimeError::RuntimeError()
+#if __cplusplus >= 201103L
+    : m_error{"RuntimeError()"}
+#else
+    : m_error("RuntimeError()")
+#endif
+  {}
 
-    i32 InvalidArgument::errorCode() const noexcept
-    {
-      return 2;
-    }
+  RuntimeError::RuntimeError(const char* errorMessage)
+#if __cplusplus >= 201103L
+    : m_error{errorMessage}
+#else
+    : m_error(errorMessage)
+#endif
+  {}
 
-    /*
-     * Runtime error implementation
-     */
-    RuntimeError::RuntimeError()
-      : m_error{"RuntimeError()"}
-    {}
+  RuntimeError::RuntimeError(const std::string& errorMessage)
+#if __cplusplus >= 201103L
+    : m_error{errorMessage}
+  {}
+#else
+    : m_error(errorMessage)
+  {}
 
-    RuntimeError::RuntimeError(const char* errorMessage)
-      : m_error{errorMessage}
-    {}
+  RuntimeError::RuntimeError(const RuntimeError& other)
+    : m_error(other.m_error)
+  {}
+#endif
 
-    RuntimeError::RuntimeError(const std::string& errorMessage)
-      : m_error{errorMessage}
-    {}
+#if __cplusplus >= 201103L
+  RuntimeError::RuntimeError(std::string&& errorMessage) __NOP_ATTRIBUTE_NOEXCEPT__
+    : m_error{std::move(errorMessage)}
+  {}
+#endif
 
-    RuntimeError::RuntimeError(std::string&& errorMessage) noexcept
-      : m_error{std::move(errorMessage)}
-    {}
+  const char* RuntimeError::what() const __NOP_ATTRIBUTE_NOEXCEPT__
+  {
+    return m_error.c_str();
+  }
 
-    const char* RuntimeError::what() const noexcept
-    {
-      return m_error.c_str();
-    }
+  i32 RuntimeError::errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__
+  {
+    return 3;
+  }
 
-    i32 RuntimeError::errorCode() const noexcept
-    {
-      return 3;
-    }
+#if __cplusplus < 201103L
+  RuntimeError& RuntimeError::operator=(const RuntimeError& other)
+  {
+    m_error = other.m_error;
+    return *this;
+  }
+#endif
 
-    /*
-     * System error implementation
-     */
-    SystemError::SystemError()
-      : RuntimeError{"SystemError()"}
-    {}
+  /*
+    * System error implementation
+    */
+  SystemError::SystemError()
+    : RuntimeError{"SystemError()"}
+  {}
 
-    SystemError::SystemError(const char* errorMessage)
-      : RuntimeError{errorMessage}
-    {}
+  SystemError::SystemError(const char* errorMessage)
+    : RuntimeError{errorMessage}
+  {}
 
-    SystemError::SystemError(const std::string& errorMessage)
-      : RuntimeError{errorMessage}
-    {}
+  SystemError::SystemError(const std::string& errorMessage)
+    : RuntimeError{errorMessage}
+  {}
 
-    SystemError::SystemError(std::string&& errorMessage) noexcept
-      : RuntimeError{std::move(errorMessage)}
-    {}
+  SystemError::SystemError(std::string&& errorMessage) noexcept
+    : RuntimeError{std::move(errorMessage)}
+  {}
 
-    i32 SystemError::errorCode() const noexcept
-    {
-      return 4;
-    }
+  i32 SystemError::errorCode() const noexcept
+  {
+    return 4;
+  }
 
-    /*
-     * Bad alloc implementation
-     */
-    BadAlloc::BadAlloc()
-      : RuntimeError{"BadAlloc()"}
-    {}
+  /*
+   * Bad alloc implementation
+   */
+  BadAlloc::BadAlloc()
+    : RuntimeError{"BadAlloc()"}
+  {}
 
-    BadAlloc::BadAlloc(const char* errorMessage)
-      : RuntimeError{errorMessage}
-    {}
+  BadAlloc::BadAlloc(const char* errorMessage)
+    : RuntimeError{errorMessage}
+  {}
 
-    BadAlloc::BadAlloc(const std::string& errorMessage)
-      : RuntimeError{errorMessage}
-    {}
+  BadAlloc::BadAlloc(const std::string& errorMessage)
+    : RuntimeError{errorMessage}
+  {}
 
-    BadAlloc::BadAlloc(std::string&& errorMessage) noexcept
-      : RuntimeError{std::move(errorMessage)}
-    {}
+#if __cplusplus >= 201103L
+  BadAlloc::BadAlloc(std::string&& errorMessage) __NOP_ATTRIBUTE_NOEXCEPT__
+    : RuntimeError{std::move(errorMessage)}
+  {}
+#endif
 
-    i32 BadAlloc::errorCode() const noexcept
-    {
-      return 5;
-    }
+  i32 BadAlloc::errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__
+  {
+    return 5;
+  }
 
-    /*
-     * Format error implementation
-     */
-    FormatError::FormatError()
-      : LogicError{"FormatError()"}
-    {}
+  /*
+   * Format error implementation
+   */
+  FormatError::FormatError()
+    : LogicError{"FormatError()"}
+  {}
 
-    FormatError::FormatError(const char* errorMessage)
-      : LogicError{errorMessage}
-    {}
+  FormatError::FormatError(const char* errorMessage)
+    : LogicError{errorMessage}
+  {}
 
-    FormatError::FormatError(const std::string& errorMessage)
-      : LogicError{errorMessage}
-    {}
+  FormatError::FormatError(const std::string& errorMessage)
+    : LogicError{errorMessage}
+  {}
 
-    FormatError::FormatError(std::string&& errorMessage) noexcept
-      : LogicError{std::move(errorMessage)}
-    {}
+  FormatError::FormatError(std::string&& errorMessage) __NOP_ATTRIBUTE_NOEXCEPT__
+    : LogicError{std::move(errorMessage)}
+  {}
 
-    i32 FormatError::errorCode() const noexcept
-    {
-      return 6;
-    }
+  i32 FormatError::errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__
+  {
+    return 6;
+  }
 
-    /*
-     * Out of range implementation
-     */
-    OutOfRange::OutOfRange()
-      : LogicError{"OutOfRange()"}
-    {}
-  
-    OutOfRange::OutOfRange(const char* errorMessage)
-      : LogicError{errorMessage}
-    {}
+  /*
+    * Out of range implementation
+    */
+  OutOfRange::OutOfRange()
+    : LogicError{"OutOfRange()"}
+  {}
 
-    OutOfRange::OutOfRange(const std::string& errorMessage)
-      : LogicError{errorMessage}
-    {}
+  OutOfRange::OutOfRange(const char* errorMessage)
+    : LogicError{errorMessage}
+  {}
 
-    OutOfRange::OutOfRange(std::string&& errorMessage) noexcept
-      : LogicError{std::move(errorMessage)}
-    {}
+  OutOfRange::OutOfRange(const std::string& errorMessage)
+    : LogicError{errorMessage}
+  {}
 
-    i32 OutOfRange::errorCode() const noexcept
-    {
-      return 7;
-    }
+#if __cplusplus >= 201103L
+  OutOfRange::OutOfRange(std::string&& errorMessage) __NOP_ATTRIBUTE_NOEXCEPT__
+    : LogicError{std::move(errorMessage)}
+  {}
+#endif
 
-    LengthError::LengthError()
-      : LogicError{"LengthError()"}
-    {}
+  i32 OutOfRange::errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__
+  {
+    return 7;
+  }
 
-    LengthError::LengthError(const char* errorMessage)
-      : LogicError{errorMessage}
-    {}
+#if __cplusplus < 201103L
+  OutOfRange& OutOfRange::operator=(const OutOfRange& other)
+  {
+    return *this;
+  }
+#endif
 
-    LengthError::LengthError(const std::string& errorMessage)
-      : LogicError{errorMessage}
-    {}
+  /*
+   * Length error implementation
+   */
+  LengthError::LengthError()
+#if __cplusplus >= 201103L
+    : LogicError{"LengthError()"}
+#else
+    : :LogicError("LogicError")
+#endif
+  {}
 
-    LengthError::LengthError(std::string&& errorMessage) noexcept
-      : LogicError{std::move(errorMessage)}
-    {}
+  LengthError::LengthError(const char* errorMessage)
+#if __cplusplus >= 201103L
+    : LogicError{errorMessage}
+#else
+    : LogicError(errorMessage)
+#endif
+  {}
 
-    i32 LengthError::errorCode() const noexcept
-    {
-      return 8;
-    }
+  LengthError::LengthError(const std::string& errorMessage)
+#if __cplusplus >= 201103L
+    : LogicError{errorMessage}
+  {}
+#else
+    : LogicError(errorMessage)
+  {}
 
-  } /* End namespace err */
+  LengthError(const LengthError& other)
+    : LogicError(other.what())
+  {}
+#endif
+
+#if __cplusplus >= 201103L
+  LengthError::LengthError(std::string&& errorMessage) __NOP_ATTRIBUTE_NOEXCEPT__
+    : LogicError{std::move(errorMessage)}
+  {}
+#endif
+
+  i32 LengthError::errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__
+  {
+    return 8;
+  }
+
+#if __cplusplus < 201103L
+  LengthError& LengthError::operator=(const LengthError& other)
+  {
+    return *this;
+  }
+#endif
+
+} /* End namespace err */
 
 } /* End namespace nop */
--- exception/exception.hpp
+++ exception/exception.hpp
@@ -1,168 +1,247 @@
-#ifndef __NOP_EXCEPTION_HPP__   /* Begin exception header file */
-#define __NOP_EXCEPTION_HPP__ 1
+#ifndef NOP_EXCEPTION_HPP /* Begin exception header file */
+#define NOP_EXCEPTION_HPP 1
 
 #include <string>
+#include "attributes.hpp"
+#include "utils.hpp"
 #include "types.hpp"
 
 namespace nop /* Begin namespace nop */
 {
 
-  namespace err /* Begin namespace err */
+namespace err /* Begin namespace err */
+{
+
+  class BaseException
   {
+  public:
+    virtual const char* what() const __NOP_ATTRIBUTE_NOEXCEPT__ = 0;
+    virtual i32 errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__ = 0;
 
-    class BaseException
-    {
-    public:
-      virtual const char* what() const noexcept = 0;
-      virtual i32 errorCode() const noexcept = 0;
-      virtual ~BaseException() = default;
-    };
+#if __cplusplus >= 201103L
+    virtual ~BaseException() = default;
+#else
+    virtual ~BaseException()
+    {}
+#endif
+  };
 
-    class LogicError : public BaseException
-    {
-    private:
-      std::string m_error;
+  class LogicError : public BaseException
+  {
+  private:
+    std::string m_error;
 
-    public:
-      LogicError();
-      LogicError(const char*);
-      LogicError(const std::string&);
-      LogicError(std::string&&) noexcept;
-      LogicError(const LogicError&) = default;
-      LogicError(LogicError&&) = default;
-      ~LogicError() = default;
+  public:
+    LogicError();
+    LogicError(const char*);
+    LogicError(const std::string&);
 
-      [[nodiscard]] const char* what() const noexcept override;
-      [[nodiscard]] i32 errorCode() const noexcept override;
+#if __cplusplus >= 201103L
+    LogicError(std::string&&) __NOP_ATTRIBUTE_NOEXCEPT__;
+    LogicError(const LogicError&) = default;
+    LogicError(LogicError&&) = default;
+    ~LogicError() = default;
+#else
+    LogicError(const LogicError&);
+#endif
 
-      LogicError& operator=(const LogicError&) = default;
-      LogicError& operator=(LogicError&&) = default;
-    };
+    __NOP_ATTRIBUTE_NODISCARD__ const char* what() const __NOP_ATTRIBUTE_NOEXCEPT__ __NOP_ATTRIBUTE_OVERRIDE__;
+    __NOP_ATTRIBUTE_NODISCARD__ i32 errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__ __NOP_ATTRIBUTE_OVERRIDE__;
 
-    class InvalidArgument : public LogicError
-    {
-    public:
-      InvalidArgument();
-      InvalidArgument(const char*);
-      InvalidArgument(const std::string&);
-      InvalidArgument(std::string&&) noexcept;
-      InvalidArgument(const InvalidArgument&) = default;
-      InvalidArgument(InvalidArgument&&) = default;
-      ~InvalidArgument() = default;
+#if __cplusplus >= 201103L
+    LogicError& operator=(const LogicError&) = default;
+    LogicError& operator=(LogicError&&) = default;
+#else
+    LogicError& operator(const LogicError&);
+#endif
+  };
 
-      [[nodiscard]] i32 errorCode() const noexcept override;
+  class InvalidArgument : public LogicError
+  {
+  public:
+    InvalidArgument();
+    InvalidArgument(const char*);
+    InvalidArgument(const std::string&);
 
-      InvalidArgument& operator=(const InvalidArgument&) = default;
-      InvalidArgument& operator=(InvalidArgument&&) = default;
-    };
+#if __cplusplus >= 201103L
+    InvalidArgument(std::string&&) __NOP_ATTRIBUTE_NOEXCEPT__;
+    InvalidArgument(const InvalidArgument&) = default;
+    InvalidArgument(InvalidArgument&&) = default;
+    ~InvalidArgument() = default;
+#else
+    InvalidArgument(const InvalidArgument&);
+#endif
 
-    class RuntimeError : public BaseException
-    {
-    private:
-      std::string m_error;
+    __NOP_ATTRIBUTE_NODISCARD__ i32 errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__ __NOP_ATTRIBUTE_OVERRIDE__;
 
-    public:
-      RuntimeError();
-      RuntimeError(const char*);
-      RuntimeError(const std::string&);
-      RuntimeError(std::string&&) noexcept;
-      RuntimeError(const RuntimeError&) = default;
-      RuntimeError(RuntimeError&&) = default;
-      ~RuntimeError() = default;
+#if __cplusplus >= 201103L
+    InvalidArgument& operator=(const InvalidArgument&) = default;
+    InvalidArgument& operator=(InvalidArgument&&) = default;
+#else
+    InvalidArgument& operator=(const InvalidArgument&);
+#endif
+  };
 
-      [[nodiscard]] const char* what() const noexcept override;
-      [[nodiscard]] i32 errorCode() const noexcept override;
+  class RuntimeError : public BaseException
+  {
+  private:
+    std::string m_error;
 
-      RuntimeError& operator=(const RuntimeError&) = default;
-      RuntimeError& operator=(RuntimeError&&) = default;
-    };
+  public:
+    RuntimeError();
+    RuntimeError(const char*);
+    RuntimeError(const std::string&);
 
-    class SystemError : public RuntimeError
-    {
-    public:
-      SystemError();
-      SystemError(const char*);
-      SystemError(const std::string&);
-      SystemError(std::string&&) noexcept;
-      SystemError(const SystemError&) = default;
-      SystemError(SystemError&&) = default;
-      ~SystemError() = default;
+#if __cplusplus >= 201103L
+    RuntimeError(std::string&&) noexcept;
+    RuntimeError(const RuntimeError&) = default;
+    RuntimeError(RuntimeError&&) = default;
+    ~RuntimeError() = default;
+#else
+    RuntimeError(const RuntimeError&);
+#endif
 
-      [[nodiscard]] i32 errorCode() const noexcept override;
+    __NOP_ATTRIBUTE_NODISCARD__ const char* what() const __NOP_ATTRIBUTE_NOEXCEPT__ __NOP_ATTRIBUTE_OVERRIDE__;
+    __NOP_ATTRIBUTE_NODISCARD__ i32 errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__ __NOP_ATTRIBUTE_OVERRIDE__;
 
-      SystemError& operator=(const SystemError&) = default;
-      SystemError& operator=(SystemError&&) = default;
-    };
+#if __cplusplus >= 201103L
+    RuntimeError& operator=(const RuntimeError&) = default;
+    RuntimeError& operator=(RuntimeError&&) = default;
+#else
+    RuntimeError& operator=(const RuntimeError&);
+#endif
+  };
 
-    class BadAlloc : public RuntimeError
-    {
-    public:
-      BadAlloc();
-      BadAlloc(const char*);
-      BadAlloc(const std::string&);
-      BadAlloc(std::string&&) noexcept;
-      BadAlloc(const BadAlloc&) = default;
-      BadAlloc(BadAlloc&&) = default;
-      ~BadAlloc() = default;
+  class SystemError : public RuntimeError
+  {
+  public:
+    SystemError();
+    SystemError(const char*);
+    SystemError(const std::string&);
 
-      [[nodiscard]] i32 errorCode() const noexcept override;
+#if __cplusplus >= 201103L
+    SystemError(std::string&&) noexcept;
+    SystemError(const SystemError&) = default;
+    SystemError(SystemError&&) = default;
+    ~SystemError() = default;
+#else
+    SystemError(const SystemError&);
+#endif
 
-      BadAlloc& operator=(const BadAlloc&) = default;
-      BadAlloc& operator=(BadAlloc&&) = default;
-    };
+    __NOP_ATTRIBUTE_NODISCARD__ i32 errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__ __NOP_ATTRIBUTE_OVERRIDE__;
 
-    class FormatError : public LogicError
-    {
-    public:
-      FormatError();
-      FormatError(const char*);
-      FormatError(const std::string&);
-      FormatError(std::string&&) noexcept;
-      FormatError(const FormatError&) = default;
-      FormatError(FormatError&&) = default;
-      ~FormatError() = default;
+#if __cplusplus >= 201103L
+    SystemError& operator=(const SystemError&) = default;
+    SystemError& operator=(SystemError&&) = default;
+#else
+    SystemError& operator=(const SystemError&);
+#endif
+  };
 
-      [[nodiscard]] i32 errorCode() const noexcept override;
+  class BadAlloc : public RuntimeError
+  {
+  public:
+    BadAlloc();
+    BadAlloc(const char*);
+    BadAlloc(const std::string&);
 
-      FormatError& operator=(const FormatError&) = default;
-      FormatError& operator=(FormatError&&) = default;
-    };
+#if __cplusplus >= 201103L
+    BadAlloc(std::string&&) noexcept;
+    BadAlloc(const BadAlloc&) = default;
+    BadAlloc(BadAlloc&&) = default;
+    ~BadAlloc() = default;
+#else
+    BadAlloc(const BadAlloc&);
+#endif
 
-    class OutOfRange : public LogicError
-    {
-    public:
-      OutOfRange();
-      OutOfRange(const char*);
-      OutOfRange(const std::string&);
-      OutOfRange(std::string&&) noexcept;
-      OutOfRange(const OutOfRange&) = default;
-      OutOfRange(OutOfRange&&) = default;
-      ~OutOfRange() = default;
+    __NOP_ATTRIBUTE_NODISCARD__ i32 errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__ __NOP_ATTRIBUTE_OVERRIDE__;
 
-      [[nodiscard]] i32 errorCode() const noexcept override;
-      
-      OutOfRange& operator=(const OutOfRange&) = default;
-      OutOfRange& operator=(OutOfRange&&) = default;
-    };
+#if __cplusplus >= 201103L
+    BadAlloc& operator=(const BadAlloc&) = default;
+    BadAlloc& operator=(BadAlloc&&) = default;
+#else
+    BadAlloc& operator=(const BadAlloc&);
+#endif
+  };
 
-    class LengthError : public LogicError
-    {
-    public:
-      LengthError();
-      LengthError(const char*);
-      LengthError(const std::string&);
-      LengthError(std::string&&) noexcept;
-      LengthError(const LengthError&) = default;
-      LengthError(LengthError&&) = default;
-      ~LengthError() = default;
+  class FormatError : public LogicError
+  {
+  public:
+    FormatError();
+    FormatError(const char*);
+    FormatError(const std::string&);
+#if __cplusplus >= 201103L
+    FormatError(std::string&&) __NOP_ATTRIBUTE_NOEXCEPT__;
+    FormatError(const FormatError&) = default;
+    FormatError(FormatError&&) = default;
+    ~FormatError() = default;
+#else
+    FormatError(const FormatError&);
+#endif
 
-      [[nodiscard]] i32 errorCode() const noexcept override;
-      
-      LengthError& operator=(const LengthError&) = default;
-      LengthError& operator=(LengthError&&) = default;
-    };
+    __NOP_ATTRIBUTE_NODISCARD__ i32 errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__ __NOP_ATTRIBUTE_OVERRIDE__;
 
-  } /*End namespace err */
+#if __cplusplus >= 201103L
+    FormatError& operator=(const FormatError&) = default;
+    FormatError& operator=(FormatError&&) = default;
+#else
+    FormatError& operator=(const FormatError&);
+#endif
+  };
+
+  class OutOfRange : public LogicError
+  {
+  public:
+    OutOfRange();
+    OutOfRange(const char*);
+    OutOfRange(const std::string&);
+
+#if __cplusplus >= 201103L
+    OutOfRange(std::string&&) noexcept;
+    OutOfRange(const OutOfRange&) = default;
+    OutOfRange(OutOfRange&&) = default;
+    ~OutOfRange() = default;
+#else
+    OutOfRange(const OutOfRange&);
+#endif
+
+    __NOP_ATTRIBUTE_NODISCARD__ i32 errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__ __NOP_ATTRIBUTE_OVERRIDE__;
+
+#if __cplusplus >= 201103L
+    OutOfRange& operator=(const OutOfRange&) = default;
+    OutOfRange& operator=(OutOfRange&&) = default;
+#else
+    OutOfRange& operator=(const OutOfRange&);
+#endif
+  };
+
+  class LengthError : public LogicError
+  {
+  public:
+    LengthError();
+    LengthError(const char*);
+    LengthError(const std::string&);
+
+#if __cplusplus >= 201103L
+    LengthError(std::string&&) noexcept;
+    LengthError(const LengthError&) = default;
+    LengthError(LengthError&&) = default;
+    ~LengthError() = default;
+#else
+    LengthError(const LengthError&);
+#endif
+
+    __NOP_ATTRIBUTE_NODISCARD__ i32 errorCode() const __NOP_ATTRIBUTE_NOEXCEPT__ __NOP_ATTRIBUTE_OVERRIDE__;
+
+#if __cplusplus >= 201103L
+    LengthError& operator=(const LengthError&) = default;
+    LengthError& operator=(LengthError&&) = default;
+#else
+    LengthError& operator=(const LengthError&);
+#endif
+  };
+
+} /*End namespace err */
 
 } /* End namespace nop */
 
