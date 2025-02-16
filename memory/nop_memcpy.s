faf24a4ae0dcf0d872acf68e7e06e5f335883cc4
podumai <kirillsm05@gmail.com>
2025-02-16 19:07:12 +0700

    Small improvement.

--- memory/nop_memcpy.s
+++ memory/nop_memcpy.s
@@ -1,33 +1,35 @@
 .globl nop_memcpy
 
+.equ NULLPTR_CONSTANT, 0
+.equ AVX512_STEP,      64
+.equ TEMP_VAR,        -8
+
 .section .text
 nop_memcpy:
   pushq %rbp
   movq %rsp, %rbp
 
-  cmpq $0, %rdi
+  cmpq $NULLPTR_CONSTANT, %rdi
   jz .L0
-  cmpq $0, %rsi
+  cmpq $NULLPTR_CONSTANT, %rsi
   jz .L0
 
-  subq $24, %rsp
-  movq %rcx, -8(%rbp)
-  movq %rdi, -24(%rbp)
+  subq $8, %rsp
 
   movq %rdx, %rcx
   shrq $6, %rcx
-  movq %rcx, -16(%rbp)
-  shlq $6, -16(%rbp)
-  subq -16(%rbp), %rdx
-  movq %rsi, -16(%rbp)
+  movq %rcx, TEMP_VAR(%rbp)
+  shlq $6, TEMP_VAR(%rbp)
+  subq TEMP_VAR(%rbp), %rdx
+  movq %rsi, TEMP_VAR(%rbp)
 
 .L1:
   cmpq $0, %rcx
   jz .L2
   vmovups (%rsi), %zmm0
   vmovups %zmm0, (%rdi)
-  addq $64, %rsi
-  addq $64, %rdi
+  addq $AVX512_STEP, %rsi
+  addq $AVX512_STEP, %rdi
   decq %rcx
   jmp .L1
 .L2:
@@ -36,11 +38,8 @@ nop_memcpy:
   cld
   rep movsb
 
-  movq -8(%rbp), %rcx
-  movq -16(%rbp), %rsi
-  movq -24(%rbp), %rdi
-  movq %rsi, %rax
-  addq $24, %rsp
+  movq TEMP_VAR(%rbp), %rax
+  addq $8, %rsp
 .L0:
   leave
   ret
