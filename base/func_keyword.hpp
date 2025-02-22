60834935860d176e15edbb1c8826da2d7715dce9
podumai <kirillsm05@gmail.com>
2025-02-22 20:00:56 +0700

    New define for convenient function signature.

---
+++ base/func_keyword.hpp
@@ -0,0 +1,8 @@
+#ifndef NOP_BASE_FUNC_KEYWORD_HPP /* Begin base func_keyword header file */
+#define NOP_BASE_FUNC_KEYWORD_HPP 1UL
+
+#pragma once
+
+#define func auto
+
+#endif /* End base func_keyword header file */
